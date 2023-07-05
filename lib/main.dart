f6c94dddbe02a6866a11aa6c9c42ea6f2e8d1d8a
HACCP92 <111282517+HACCP92@users.noreply.github.com>
2023-07-05 19:54:51 +0900

    [Api 응용 앱] json data 변환

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mask/model/store.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,22 +32,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final List<Store> stores = [];
   Future<void> fetch() async {
     var url =
         'https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json?lat=37.266389&lng=126.999333&m=5000';
 
     var response = await http.get(Uri.parse(url));
+    final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
+    final jsonStores = jsonResult['stores'];
+
+    stores.clear();
+    jsonStores.forEach((e) {
+      stores.add(Store.fromJson(e));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('title'),
+        title: const Text('마스크 재고 있는 곳 : 0곳'),
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: fetch,
+          onPressed: () async {
+            await fetch();
+          },
           child: const Text('테스트'),
         ),
       ),
